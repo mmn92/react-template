35ebf234480edb47285f8a1573b0b9f208822585
mmn92 <mmn92>
2022-04-19 21:33:47 -0300

    create template routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "../../Routes";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello there</h1>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </div>
   );
 }
