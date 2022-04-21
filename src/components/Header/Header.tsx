cbac66ed79a0cec36ef8153b937d47c2d5b22079
mmn92 <mmn92>
2022-04-20 23:07:16 -0300

    create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import "./styles.scss";
+
+export function Header() {
+  return (
+    <header className="application-header">
+      <h1 className="title">React Template</h1>
+    </header>
+  );
+}
